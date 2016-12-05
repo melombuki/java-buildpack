1e930a27c53ab98cb1fb5ae07e93604ab3c8243d
Josh Hemen <josh.hemen@schwab.com>
2016-12-05 12:37:48 -0600

    More hacking.

--- lib/java_buildpack/container/java_main.rb
+++ lib/java_buildpack/container/java_main.rb
@@ -83,7 +83,7 @@ module JavaBuildpack
           classpath,
           main_class,
           arguments,
-          "export PATH=/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH"
+          "&& export PATH=/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH"
         ].flatten.compact.join(' ')
       end
 
--- lib/java_buildpack/framework/phantom_js.rb
+++ lib/java_buildpack/framework/phantom_js.rb
@@ -42,10 +42,6 @@ module JavaBuildpack
         @droplet.environment_variables.add_environment_variable 'PATH', "/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH"
       end
 
-      def command
-        "export PATH=/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH"
-      end
-
     end
   end
 end
