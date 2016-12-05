59f43e071ab05b18ce7f6a076e9be02b82a7d6ec
Josh Hemen <josh.hemen@schwab.com>
2016-12-05 09:36:42 -0600

    More hacking.

---
+++ lib/java_buildpack/framework/phantom_js.rb
@@ -0,0 +1,51 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2016 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'java_buildpack/component/base_component'
+require 'java_buildpack/framework'
+require 'java_buildpack/util/dash_case'
+require 'shellwords'
+
+module JavaBuildpack
+  module Framework
+
+    # Encapsulates the functionality for contributing custom Java options to an application.
+    class PhantomJS < JavaBuildpack::Component::BaseComponent
+
+      # (see JavaBuildpack::Component::BaseComponent#detect)
+      def detect
+        "phantom_js"
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#compile)
+      def compile
+        copy_resources
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#release)
+      def release
+      end
+
+      private
+
+      ENVIRONMENT_VARIABLE = 'PATH'.freeze
+
+      private_constant :ENVIRONMENT_VARIABLE
+
+    end
+
+  end
+end
