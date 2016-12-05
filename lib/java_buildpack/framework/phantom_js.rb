7b65bed75b98a94166e8925316603bd69c6c7c2a
Josh Hemen <josh.hemen@schwab.com>
2016-12-05 17:04:20 -0600

    More hacking.

--- lib/java_buildpack/framework/phantom_js.rb
+++ lib/java_buildpack/framework/phantom_js.rb
@@ -39,7 +39,7 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        @droplet.environment_variables.add_environment_variable 'PATH', "/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH"
+        "source #{@droplet.sandbox}/phantom_js.sh"
       end
 
     end
---
+++ resources/phantom_js/phanton_js.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export PATH=/home/vcap/app/.java-buildpack/phantom_js/phantomjs-2.1.1-linux-x86_64/bin:$PATH
